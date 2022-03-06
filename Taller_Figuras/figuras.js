22b0a7372295585a644fb765b375fe0c01891a25
Manuel Cruz <manuelcruzg78@gmail.com>
2022-03-06 11:39:17 -0600

    se finaliza las interacciones de html con js en las figuras de cuadrado y circulo. Queda faltando el area y perimetro

--- Taller_Figuras/figuras.js
+++ Taller_Figuras/figuras.js
@@ -3,7 +3,7 @@ console.group("Calculos cuadrado")
 function PerimetroCuadrado (lado) {
     return lado * 4;
 }
-function AreacCuadrado (lado) {
+function AreaCuadrado (lado) {
     return lado * lado;
 }
 console.groupEnd()
@@ -26,4 +26,36 @@ function PerimetroCirculo(diametro) {
 }
 function AreaCirculo(radio) {
     return (radio * radio) * Math.PI;
+}
+
+// Interacciones con Html
+function CalcularPerimetroCuadrado() {
+    const input = document.getElementById("SquareInput");
+    const value = input.value;
+    const perimetro = PerimetroCuadrado(value)
+
+    alert(perimetro)
+}
+function CalcularAreaCuadrado() {
+    const input = document.getElementById("SquareInput");
+    const value = input.value;
+    const area = AreaCuadrado(value);
+
+    alert(area)
+}
+// Faltan calculos de triangulo
+
+function CalcularPerimetroCirculo() {
+    const input = document.getElementById("RadioInput");
+    const value = input.value;
+    const perimetro = PerimetroCirculo(value)
+
+    alert(perimetro)
+}
+function CalcularAreaCirculo() {
+    const input = document.getElementById("RadioInput");
+    const value = input.value;
+    const area = AreaCirculo(value)
+
+    alert(area)
 }
